ca1891d54074fc39e197e97b1aad6047caec31e2
Local Body <localbody@MacBook-Air-Local.local>
2020-07-17 10:48:15 +0300

    mobile and index-desktop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(function(){
+$('.header__menu-btn').click(
+    () => {
+        $('.header__menu-btn').toggleClass('header__menu-btn--close')
+        $('.header__menu').toggleClass('nav--show')
+    })
 
-});
+$('.faq-title').click(
+    function(event) {
+        $('.faq-title').not($(this)).removeClass('faq-title--active')
+        $('.faq-text').not($(this).next()).slideUp(300)
+        $(this).toggleClass('faq-title--active').next().slideToggle(300)
+    })
