2a81717c28b17dc19c7ccd1072c9cfec575b40d8
Local Body <localbody@MacBook-Air-Local.local>
2020-07-31 21:16:35 +0300

    evening 31.07.2020

--- quiz/entity/q5.inc.php
+++ quiz/entity/q5.inc.php
@@ -14,9 +14,6 @@
     <input type="hidden" name="question" id="question" value="<?=$nextQuestion?>">
     <input type="hidden" name="questions" id="questions" value="34">
     <ul class="quiz__answers-list">
-        <li class="quiz__answers-item">
-            <input class="form__name bg--light" type="email" name="email" id="email" placeholder="Ваш E-Mail">
-        </li>
 
         <li class="quiz__answers-item">
             <label class="quiz__answers-label">
