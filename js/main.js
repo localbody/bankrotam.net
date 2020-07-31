1f9ed9f9c203a0f4801dab05973e925c37d9c455
Local Body <localbody@MacBook-Air-Local.local>
2020-07-31 20:06:51 +0300

    2020-07-31 evening

--- js/main.js
+++ js/main.js
@@ -1,71 +1,90 @@
-$('.header__menu-btn').click(
-    () => {
-        $('.header__menu-btn').toggleClass('header__menu-btn--close')
-        $('.header__menu').toggleClass('nav--show')
-    })
+$(".header__menu-btn").click(() => {
+    $(".header__menu-btn").toggleClass("header__menu-btn--close")
+    $(".header__menu").toggleClass("nav--show")
+})
 
-$('.faq-title').click(
-    function(event) {
-        $('.faq-title').not($(this)).removeClass('faq-title--active')
-        $('.faq-text').not($(this).next()).slideUp(300)
-        $(this).toggleClass('faq-title--active').next().slideToggle(300)
-    })
+$(".faq-title").click(function (event) {
+    $(".faq-title").not($(this)).removeClass("faq-title--active")
+    $(".faq-text").not($(this).next()).slideUp(300)
+    $(this).toggleClass("faq-title--active").next().slideToggle(300)
+})
 
 // popup
 
-const popups__feedback = $('.popup__feedback-link')
+const popups__feedback = $(".popup__feedback-link")
 if (popups__feedback.length > 0) {
-
-
     for (let index = 0; index < popups__feedback.length; index++) {
         const element = popups__feedback[index];
 
-        $(element).click(function(e) {
+        $(element).click(function (e) {
             console.log(1)
 
-            $('.popup__feedback').addClass('open')
+            $(".popup__feedback").addClass("open")
             e.preventDefault()
         })
     }
 }
 
-const popups__thanks = $('.popup__thanks-link')
+const popups__thanks = $(".popup__thanks-link")
 if (popups__thanks.length > 0) {
     for (let index = 0; index < popups__thanks.length; index++) {
         const element = popups__thanks[index];
 
-        $(element).click(function(e) {
-
+        $(element).click(function (e) {
             // закроем другие попапы
-            $('.popup').removeClass('open')
+            $(".popup").removeClass("open")
 
-            $('.popup__thanks').addClass('open')
+            $(".popup__thanks").addClass("open")
             e.preventDefault()
         })
     }
 }
 
-
-$('.popup__close').click(function(e) {
-    $('.popup').removeClass('open')
+$(".popup__close").click(function (e) {
+    $(".popup").removeClass("open")
     e.preventDefault()
 })
 
 const maxScroll = 1585 - $(document).width()
 
-$('.stages__items').on('mousewheel', function(event) {
-
+$(".stages__items").on("mousewheel", function (event) {
     console.log(1)
 
-    var scrollLeft = $('.stages__items').scrollLeft()
-    var scrollValue = scrollLeft - (event.deltaY * event.deltaFactor)
-    $('.stages__items').scrollLeft(scrollValue)
-    if ((scrollLeft > 0) && (scrollLeft < maxScroll)) {
+    var scrollLeft = $(".stages__items").scrollLeft()
+    var scrollValue = scrollLeft - event.deltaY * event.deltaFactor;
+    $(".stages__items").scrollLeft(scrollValue)
+    if (scrollLeft > 0 && scrollLeft < maxScroll) {
         event.preventDefault()
     }
-});
+})
 
+const question = $("#question").val()
+const questions = $("#questions").val()
+const ready = Math.round((99 / questions) * question)
 
-$(function() {
-    $(".phone").mask("+7 (999) 999-99-99");
-});
+$(".quiz__progressbar-count").html("Вопрос " + question + " из " + questions)
+$(".quiz__progressbar-percent").html(ready + "%")
+$(".quiz__progressbar-process").attr(
+    "style",
+    "grid-column: 1 / " + ready + " ;"
+)
+
+$(".quiz__subtitle").html("Вопрос " + question)
+
+const actionLink = question != questions ? (parseInt(question)+1) + ".html" : "result.html"
+
+if (question == questions) 
+{
+    $(".quiz__form-btn-next").html("Завершить опрос")
+}
+
+$(".quiz__form-btn-next").click( function() {
+    $(".quiz__form").attr("action", actionLink+"#quiz")
+} )
+
+const href = document.location.href
+const anchor = href.substr(href.lastIndexOf('#') + 1)
+
+if (anchor == "quiz") {
+    $('html').animate({ scrollTop: $(".quiz").offset().top - 140 }, 300);
+}
