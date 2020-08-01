47c3b92df822ca84047a98fe4acea8ba23bd8412
Local Body <localbody@MacBook-Air-Local.local>
2020-08-01 22:26:44 +0300

    2020-08-01 nightly

--- feedback.php
+++ feedback.php
@@ -4,10 +4,11 @@
 
     $name   = "Имя: " . isset($_GET["name"]) ? $_GET["name"] : "Без имени";
     $phone  = "Телефон: " . isset($_GET["phone"]) ? $_GET["phone"] : "Без телефона";
-    $summa  = "Сумма: " . isset($_GET["summa"]) ? $_GET["summa"] : "Сумма не указана";
+    $summa  = "Сумма долга: " . isset($_GET["summa"]) ? $_GET["summa"] : "Сумма не указана";
+    $text   = "Сообщение: " . isset($_GET["text"]) ? $_GET["text"] : "Без текста";
     
     // Сообщение
-    $message = "$name\r\n$phone\r\n$summa";
+    $message = "$name\r\n$phone\r\n$summa\r\n$text";
     // На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
     $message = wordwrap($message, 70, "\r\n");
 
