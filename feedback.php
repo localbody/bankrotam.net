f73c7a442846b34e3ceeebc598f26b6f439a2725
Local Body <localbody@MacBook-Air-Local.local>
2020-08-02 00:57:57 +0300

    2020-08-01 nightly

--- feedback.php
+++ feedback.php
@@ -3,13 +3,13 @@
     $subject = "Обратная связь с BANKROTAM.NET";
 
     $name   = "Имя: "; 
-    $name   .= isset($_POST["name"]) ? $_POST["name"] : "Без имени";
+    $name   .= (isset($_POST["name"]) && !empty($_POST["name"])) ? $_POST["name"] : "Без имени";
     $phone  = "Телефон: ";
-    $phone  .= isset($_POST["phone"]) ? $_POST["phone"] : "Без телефона";
+    $phone  .= (isset($_POST["phone"]) && !empty($_POST["phone"])) ? $_POST["phone"] : "Без телефона";
     $summa  = "Сумма долга: ";
-    $summa  .= "Сумма долга: " . isset($_POST["summa"]) ? $_POST["summa"] : "Сумма не указана";
+    $summa  .= (isset($_POST["summa"]) && !empty($_POST["summa"])) ? $_POST["summa"] : "Сумма не указана";
     $text   = "Сообщение: ";
-    $text   .= isset($_POST["text"]) ? $_POST["text"] : "Без текста";
+    $text   .= (isset($_POST["text"]) && !empty($_POST["text"])) ? $_POST["text"] : "Без текста";
     
     // Сообщение
     $message = "$name\r\n$phone\r\n$summa\r\n$text";
