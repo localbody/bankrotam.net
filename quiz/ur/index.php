13cb0cc42f4adde11614b7fba2bf03b43a475aa0
Local Body <localbody@MacBook-Air-Local.local>
2020-08-02 00:19:15 +0300

    2020-08-01 nightly

--- quiz/ur/index.php
+++ quiz/ur/index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="description" content="Пройдите опросник и заполните форму чтобы мы смогли вам помочь. Дайте ответ на несколько вопросов и мы с вами свяжемся.">
+    <title>Форма заполнения онлайн заявки на банкротство юридического лица</title>
     <link rel="stylesheet" href="../../css/common.css">
     <link rel="stylesheet" href="../../css/quiz.css">
 
