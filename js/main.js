5fe878b1d51bfd9a95fa08498dc9a93a1518f0d4
Local Body <localbody@MacBook-Air-Local.local>
2020-08-02 03:55:46 +0300

    2020-08-02 nightly

--- js/main.js
+++ js/main.js
@@ -213,7 +213,7 @@ $(".quiz__form-btn-next").click(function (e) {
     } else {
         // собираем ответы
         let questionText = $(".quiz__title").html()
-        let questionAnswer = $("input:checked.check__input").html()
+        let questionAnswer = $(".check__input:checked").html()
         
         localStorage.setItem('q'+question.toString(), questionText)
         localStorage.setItem('a'+question.toString(), questionAnswer)
